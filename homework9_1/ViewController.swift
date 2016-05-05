e0b62b977d2013bea81683acddce1c11a655d152
MikeWu <MikeWu@mikewude-macbook-pro.local>
2016-05-06 00:23:23 +0800

    在畫面上加入2個text field，分別代表帳號和密碼，從帳號的text field按鍵盤的next跳到下一個text field，從密碼是text field是return，按了return 收下來

--- homework9_1/ViewController.swift
+++ homework9_1/ViewController.swift
@@ -1,14 +1,22 @@
-//
-//  ViewController.swift
-//  homework9_1
-//
-//  Created by MikeWu on 2016/5/5.
-//  Copyright © 2016年 Mike. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
+
+
+
+    @IBOutlet var accountTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        if textField === self.accountTextField {
+            self.passwordTextField.becomeFirstResponder()
+        }
+        else {
+            self.resignFirstResponder() //resign是收鍵盤
+        }
+        return true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,3 +31,9 @@ class ViewController: UIViewController {
 
 }
 
+/*
+ step1. 要在viewcontroller加上UITextFieldDelegate的protocol
+ step2. 將兩個UITextField的元件拉到viewcontroller，將它們與viewcontroller有關聯(delegte)
+ step3. 收鍵盤的func是func textFieldShouldReturn(textField: UITextFiled) -> Bool {}
+ step4. 在裡面用if條件判斷textField是否是等於accountTextField這個格子裡，如果是的話，就執行讓passwordTextField這格子變成是與使用者第一互動的元件(becomeFirstResponder)
+ */
